e689c9adfd24db7c2deeebc03bda5eea4c9821b8
samanthakeb <142178171+samanthakeb@users.noreply.github.com>
2024-02-14 21:28:40 -0800

    Add files via upload

--- boot.bat
+++ boot.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p answer=Would you like to play Rock Paper Scissors? (yes/no): 
 if /i "%answer%"=="yes" (
-   start "Game File" "C:\Users\saman\OneDrive\Documents\rock_paper_scissors.py"
+   python rock_paper_scissors.py
 ) else (
     echo That's too bad, maybe next time.
 )
